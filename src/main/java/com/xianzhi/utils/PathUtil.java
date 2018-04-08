4d1ef42f9ff8d0d5984e8cefcb7b12cd45ecf82b
robin <756517553@qq.com>
2018-04-08 18:36:08 +0800

    change path

--- src/main/java/com/xianzhi/utils/PathUtil.java
+++ src/main/java/com/xianzhi/utils/PathUtil.java
@@ -17,7 +17,7 @@ public class PathUtil {
 		String basePath = "";
 		String osName = System.getProperty("os.name");
 		if (osName.toLowerCase().startsWith("win")) {
-			basePath = "D:/xianzhi/image";
+			basePath = "/xianzhi/image";
 		} else {
 			basePath = "/home/xianzhi/image";
 		}
